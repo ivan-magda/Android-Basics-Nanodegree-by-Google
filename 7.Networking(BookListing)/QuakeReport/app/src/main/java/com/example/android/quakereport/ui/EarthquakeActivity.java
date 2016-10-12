c6dec4c9ea0cc154d9ca4665de9ae634ffbc6ae9
Ivan Magda <imagda15@gmail.com>
2016-10-12 20:03:02 +0900

    [Networking] Complete last lesson in the Networking course.

--- 7.Networking(BookListing)/QuakeReport/app/src/main/java/com/example/android/quakereport/ui/EarthquakeActivity.java
+++ 7.Networking(BookListing)/QuakeReport/app/src/main/java/com/example/android/quakereport/ui/EarthquakeActivity.java
@@ -18,10 +18,14 @@ package com.example.android.quakereport.ui;
 import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -41,7 +45,7 @@ public class EarthquakeActivity extends AppCompatActivity
     /**
      * URL for earthquake data from the USGS dataset
      */
-    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query";
 
     /**
      * Constant value for the earthquake loader ID. We can choose any integer.
@@ -82,6 +86,25 @@ public class EarthquakeActivity extends AppCompatActivity
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private void openEarthquakeWebsite(Earthquake earthquake) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(earthquake.getUrl()));
@@ -95,7 +118,26 @@ public class EarthquakeActivity extends AppCompatActivity
     @Override
     public Loader<ArrayList<Earthquake>> onCreateLoader(int id, Bundle args) {
         Log.d(LOG_TAG, "On create loader");
-        return new EarthquakeLoader(EarthquakeActivity.this, USGS_REQUEST_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
